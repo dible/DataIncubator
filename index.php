3672b3405557d1e68aab7b9377632c45b482d166
dible <dible@users.noreply.github.com>
2016-01-31 22:31:48 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+echo ' 
+<!DOCTYPE html>
+<html>
+<img src="https://cloud.githubusercontent.com/assets/1903898/12707981/8845d76a-c868-11e5-8cdb-aca8b7d6a1c9.png" alt="first plot">
+
+</html>
+';
+?>
